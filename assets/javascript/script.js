68e97ec4f8b341cae21af8c52272acfc58af6cc4
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-09 22:42:09 -0600

    random number edit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     var sound;
 
     loadNewGame = function () {
-        randomTarget = Math.floor((Math.random() * 100) + 19);
+        randomTarget = Math.floor((Math.random() * 120) + 19);
         console.log(randomTarget);
 
         $("#target-value").text("Target Number: " + randomTarget);
@@ -60,34 +60,3 @@ $(document).ready(function () {
 
 
 
-//1 create caracters as objects
-//a player 1 
-//hp
-//ap
-//cap
-//b player 1 
-//hp
-//ap
-//cap 
-//c player 1 
-//hp
-//ap
-//cap 
-//d player 1 
-//hp
-//ap
-//cap   
-
-//1.5 when a character attacks, its ap goes up incrementaly.
-
-//2 when one chrater is selected, the rest are moved to enemies possition
-
-//3 when one enemy is selected, it is moved to the defender possition
-
-//4 when a character attacks, it removes hp from the defender, and the defender responds by removing hp from the chracter
-
-//5 when any character gets to 0 or less hp, the charcter is defeated
-
-//6 when a defnding character is defeted, the image goes away
-
-//7 if the selected character defeats the other three, the user wins.
